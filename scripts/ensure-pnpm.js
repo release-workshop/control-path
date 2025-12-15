15c657754f680611c86c22713cf029ed450ad212
Chris Westcott <chris@releaseworkshop.com>
2025-12-15 08:33:48 +0000

    fixes copyright notice

--- scripts/ensure-pnpm.js
+++ scripts/ensure-pnpm.js
@@ -1,7 +1,7 @@
 #!/usr/bin/env node
 
 /**
- * Copyright 2024-2025 Release Workshop Ltd
+ * Copyright 2025 Release Workshop Ltd
  * Licensed under the Elastic License 2.0; you may not use this file except in compliance with the Elastic License 2.0.
  * See the LICENSE file in the project root for details.
  */
